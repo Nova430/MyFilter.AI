e31e75a706449f27d0ff6b45d1f447e833c91926
Nova <nova@live.co.uk>
2024-04-02 19:19:29 +0100

    Tranform Delay

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -100,7 +100,11 @@ const TransformedImage = ({ image, type, title, transformationConfig, isTransfor
                   placeholder={dataUrl as PlaceholderValue}
                   className="transformed-image"
                   onLoad={() => {
-                    setIsTransformation && setIsTransformation(false);
+                    if (setIsTransformation) {
+                      setTimeout(() => {
+                        setIsTransformation(false);
+                      }, 2000); // Delay of 2000 milliseconds (2 seconds)
+                    }
                   }}
                   onError={() => {
                     debounce(() => {
